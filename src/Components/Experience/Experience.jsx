0ebe9ee88b2eb1195c6b0d50091e9c8e687caa2b
Yesica Gonzalez <yesicanicolegonzalez@gmail.com>
2022-09-08 17:12:01 -0300

    skills

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -5,18 +5,20 @@ const Experience = () => {
   return (
     <div id="experience">
         <div className="title">
-        <h2>Mis Skills</h2>
+            <h2>Mis Skills</h2>
         </div>
         <div>
-        <ul className="skills">
-            <li><i class="fa-brands fa-html5"></i></li>
-            <li><i class="fa-brands fa-css3"></i></li>
-            <li><i class="fa-brands fa-js"></i></li>
-            <li><i class="fa-brands fa-react"></i></li>
-            <li><i class="fa-brands fa-node-js"></i></li>
-            <li><i class="fa-brands fa-github"></i></li>
-            <li><i class="fa-brands fa-bootstrap"></i></li>
-        </ul>
+            <ul className="skills">
+                <li><i class="fa-brands fa-html5"></i></li>
+                <li><i class="fa-brands fa-css3"></i></li>
+                <li><i class="fa-brands fa-js"></i></li>
+                <li><i class="fa-brands fa-react"></i></li>
+                <li><i class="fa-brands fa-node-js"></i></li>
+                <li><i class="fa-brands fa-php"></i></li>
+                <li><i class="fa-brands fa-wordpress"></i></li>
+                <li><i class="fa-brands fa-bootstrap"></i></li>
+                <li><i class="fa-brands fa-github"></i></li>                
+            </ul>
         </div>
     </div>
   )
