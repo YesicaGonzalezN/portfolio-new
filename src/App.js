346eed5a4cd8412d8ca191aaa2a98b793942e543
Yesica Gonzalez <yesicanicolegonzalez@gmail.com>
2022-09-08 19:30:03 -0300

    cambios proyectos

--- src/App.js
+++ src/App.js
@@ -7,30 +7,20 @@ import Projects from "./Components/Projects/Projects";
 import ContactMe from "./Components/ContactMe/ContactMe";
 import Footer from "./Components/Footer/Footer";
 
-
 function App() {
   return (
     <div className="App">
       <Nav />
       <Main />
       <About />
-      <Experience />
       <Projects />
+      <Experience />
       <ContactMe />
       <Footer />
     </div>
   );
 }
 
-// eslint-disable-next-line no-restricted-globals
-addEventListener('DOMContentLoaded', () => {
-  const btnMenu = document.querySelector('.btn-menu')
-  if (btnMenu) {
-    btnMenu.addEventListener('click', () => {
-      const menuItems = document.querySelector('.menu-items')
-      menuItems.classList.toggle('show')
-    })
-  }
-});
+
 
 export default App;
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Projects.css';
-import Aqualife from '../../img/aqualife.png';
+import Vaso from '../../img/vaso.png';
 import Lista from '../../img/lista.png';
 import Calculadora from '../../img/calculadora.png';
+import Tilt from 'react-vanilla-tilt';
 
 const Projects = () => {
   return (
@@ -10,69 +11,40 @@ const Projects = () => {
        <div className="title">
            <h3>Proyectos</h3>
        </div>
+    <div className="cards-project">
+        <div className="card-project">
+            <h2 className="name-project">AquaLife</h2>
+            <div className="button-project">
+            <a href='https://github.com/YesicaGonzalezN/AquaLife.github.io' target='_blank' rel="noreferrer">GitHub</a>
+            <a href='https://aqualife.vercel.app/' target='_blank' rel="noreferrer">Deploy</a>
+            </div>
+            <div className="circle-project"></div>
+            <img src={Vaso} alt="Logo Aqualife" />
+        </div>
+        <div className="card-project">
+            <h2 className="name-project">Lista de Tareas</h2>
+            <div className="button-project">
+            <a href='https://github.com/YesicaGonzalezN/AquaLife.github.io' target='_blank' rel="noreferrer">GitHub</a>
+            <a href='https://aqualife.vercel.app/' target='_blank' rel="noreferrer">Deploy</a>
+            </div>
+            <div className="circle-project"></div>
+            <img src={Lista} alt="Logo Aqualife" />
+        </div>
+        <div className="card-project">
+            <h2 className="name-project">Calculadora</h2>
+            <div className="button-project">
+                <a href='https://github.com/YesicaGonzalezN/AquaLife.github.io' target='_blank' rel="noreferrer">GitHub</a>
+                <a href='https://aqualife.vercel.app/' target='_blank' rel="noreferrer">Deploy</a>
+            </div>
+            <div className="circle-project"></div>
+            <img src={Calculadora} alt="Logo Aqualife" />
+        </div>
+    </div>
+</div>
 
-       <div className="cards">
-            <div className="card">
-                <div className="img-card">
-                <img src={Aqualife} alt="Aqualife" />
-                </div>
-                <div className="content-card">
-                    <div className="details">
-                        <h2>AquaLife</h2>
-                        <h3>Aplicación Web</h3>
-                        <div className="data">
-                        <p>Aplicación Web para calcular el agua que se debe tomar diariamente. Cuenta 
-                            con un calculador de IMC.
-                        </p>
-                        </div>
-                        <div className="btn-action">
-                            <a href='https://github.com/YesicaGonzalezN/AquaLife.github.io' target='_blank' rel="noreferrer">GitHub</a>
-                            <a href='https://aqualife.vercel.app/' target='_blank' rel="noreferrer">Deploy</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="card">
-                <div className="img-card">
-                <img src={Lista} alt="Lista de Tareas" />
-                </div>
-                <div className="content-card">
-                    <div className="details">
-                        <h2>Lista de Tareas</h2>
-                        <h3>Aplicación Web</h3>
-                        <div className="data">
-                        <p>Aplicación Web para agregar notas diarias, tiene la funcionalidad de marcarlas como "realizadas" o "sin realizar".
-                        </p>
-                        </div>
-                        <div className="btn-action">
-                            <a href='https://github.com/YesicaGonzalezN/LISTA-DE-TAREAS' target='_blank' rel="noreferrer">GitHub</a>
-                            <a href='https://lista-de-tareas-two.vercel.app/' target='_blank' rel="noreferrer">Deploy</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="card">
-                <div className="img-card">
-                    <img src={Calculadora} alt="Calculadora React" />
-                </div>
-                <div className="content-card">
-                    <div className="details">
-                        <h2>Calculadora</h2>
-                        <h3>Aplicación Web</h3>
-                        <div className="data">
-                        <p>Calculadora funcional realizada puramente con React, tiene las operaciones de suma, resta, multiplicación y división.
-                        </p>
-                        </div>
-                        <div className="btn-action">
-                            <a href='https://github.com/YesicaGonzalezN/calculadora-react' target='_blank' rel="noreferrer">GitHub</a>
-                            <a href='https://calculadora-react-blond.vercel.app/' target='_blank' rel="noreferrer">Deploy</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-       </div>
-   </div>
   )
+  
 }
+
 
 export default Projects;
