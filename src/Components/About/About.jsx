44a2b2408c47ed7e0fef5f6b6970760c8e11189c
Yesica Gonzalez <yesicanicolegonzalez@gmail.com>
2022-09-09 12:07:38 -0300

    about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
             <div className="about-right">
                 <p>Hola! Soy Yesica González, tengo 27 años y soy Desarrolladora Full Stack.</p>
                 <p>Inicie en el mundo de la programación hace dos años de manera autodidacta, nunca me imaginé la pasión que me iba a generar. Por este motivo hace un año decidí empezar a certificarme para demostrar mis conocimientos y poder conseguir mi primer experiencia laboral en el increíble mundo IT.</p>
-                <p>Tengo experiencia trabajando para una agencia de Estados Unidos en el que me desempeñé como Desarrolladora Web Full Stack Trainee, en la cual utilice lenguajes y tecnologías como:</p>
+                <p>Tengo experiencia trabajando para una agencia de Estados Unidos en la que me desempeñé como Desarrolladora Web Full Stack Trainee, utilice lenguajes y tecnologías como:</p>
                 <span>HTML | CSS | JavaScript | JQuery | PHP | MySQL | Wordpress | Bootstrap</span>
             </div>
         </div>
